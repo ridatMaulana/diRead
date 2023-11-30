3e0010dae1a2a95afa29a6e8a349071997424669
ASFillah <2131710001@student.polinema.ac.id>
2023-11-30 21:45:52 +0700

    welcome, login, register

---
+++ app/src/main/java/com/mobiledev/diread/data/ui/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.mobiledev.diread.data.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mobiledev.diread.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
