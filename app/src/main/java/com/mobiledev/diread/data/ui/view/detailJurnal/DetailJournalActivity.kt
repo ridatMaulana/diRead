fbe699da6bf6295e5b0bef7d4a0969218bd95117
ASFillah <2131710001@student.polinema.ac.id>
2023-12-04 19:49:13 +0700

    collection, profile

---
+++ app/src/main/java/com/mobiledev/diread/data/ui/view/detailJurnal/DetailJournalActivity.kt
@@ -0,0 +1,12 @@
+package com.mobiledev.diread.data.ui.view.detailJurnal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mobiledev.diread.R
+
+class DetailJournalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_journal)
+    }
+}
