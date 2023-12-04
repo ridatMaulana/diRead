d1401e419bab16d1ac943b27455225f9dfcf30aa
punkiy <amuammar698@gmail.com>
2023-12-04 09:42:29 +0800

    penambahan botton barr

--- app/src/main/java/com/mobiledev/diread/data/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/mobiledev/diread/data/ui/view/main/MainActivity.kt
@@ -5,28 +5,16 @@ import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mobiledev.diread.R
+import com.mobiledev.diread.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val dummyGenres = listOf("Action", "Romance", "Mystery", "Science Fiction", "Fantasy")
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(menuItemSelected)
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        for ((index, genre) in dummyGenres.withIndex()) {
-            val menuItem = bottomNavigationView.menu.add(Menu.NONE, index, Menu.NONE, genre)
 
-        }
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                0 -> {
-                    true
-                }
-                1 -> {
-                    true
-                }
-                else -> false
-            }
-        }
     }
 }
