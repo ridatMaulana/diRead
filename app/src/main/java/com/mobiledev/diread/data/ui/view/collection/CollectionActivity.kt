0389cd475f47a674a288cdb9097febd06c411ae6
ASFillah <2131710001@student.polinema.ac.id>
2023-12-04 19:04:08 +0700

    fix homepage

---
+++ app/src/main/java/com/mobiledev/diread/data/ui/view/collection/CollectionActivity.kt
@@ -0,0 +1,12 @@
+package com.mobiledev.diread.data.ui.view.collection
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mobiledev.diread.R
+
+class CollectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collection)
+    }
+}
